2fcb812443b4ccdef2e43687245b4f38665071ee
JennyMcFlyy <jennyrambla@gmail.com>
2021-09-12 22:22:27 -0300

    subiendo fotos de productos y accesorios app.js y logo

---
+++ app.js
@@ -0,0 +1,25 @@
+const express =  require ("express");
+
+const app = express ();
+
+const path = require ("path");
+
+app.listen(3050, () =>  console.log ("Levantando servidor http://localhost:3090"));
+
+const publicFolder = path.resolve (__dirname, "./public");
+app.use(express.static(publicFolder));
+
+app.get("/", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/home.html"))
+});
+
+app.get("/register", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/register.html"))
+});
+
+app.get("/login", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/login.html"))
+});
+
+
+
