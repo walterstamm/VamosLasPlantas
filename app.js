07f453f0bb19750d8be0e87c9d954913c603ad41
JennyMcFlyy <jennyrambla@gmail.com>
2021-09-13 01:43:20 -0300

    ajustando el index

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ const app = express ();
 
 const path = require ("path");
 
-app.listen(3050, () =>  console.log ("Levantando servidor http://localhost:3090"));
+app.listen(3090, () =>  console.log ("Levantando servidor http://localhost:3090"));
 
 const publicFolder = path.resolve (__dirname, "./public");
 app.use(express.static(publicFolder));
 
 app.get("/", function (req, res){
-    res.sendFile (path.resolve (__dirname, "./views/home.html"))
+    res.sendFile (path.resolve (__dirname, "./views/index.html"))
 });
 
-app.get("/register", function (req, res){
+app.get("/register.html", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/register.html"))
 });
 
-app.get("/login", function (req, res){
+app.get("/login.html", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/login.html"))
 });
 
-app.get("/login", function (req, res){
+app.get("/login.html", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/productCart.html"))
 });
 
-app.get("/login", function (req, res){
+app.get("/login.html", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/productDetail.html"))
 });
 
