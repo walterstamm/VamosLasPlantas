5afdcb738fbe5683f76fa562b1b62d96ee159102
JennyMcFlyy <jennyrambla@gmail.com>
2021-10-05 03:24:25 -0300

    aplicando ajustes de rutas, creando archivos ejs, ajustando el app.js, ajustando estilos con Agus

--- app.js
+++ app.js
@@ -9,37 +9,66 @@ app.listen(3090, () =>  console.log ("Levantando servidor http://localhost:3090"
 const publicFolder = path.resolve (__dirname, "./public");
 app.use(express.static(publicFolder));
 
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res){
+    res.render("pages/index.ejs");})
+
+app.get("/index.ejs", function (req, res){
+    res.render("pages/index.ejs");})
+
+app.get("/register.ejs", function (req, res){
+    res.render("pages/register.ejs");})
+
+app.get("/login.ejs", function (req, res){
+    res.render("pages/login.ejs");})
+
+app.get("/cart.ejs", function (req, res){
+    res.render("pages/cart.ejs");})
+
+app.get("/product.ejs", function (req, res){
+    res.render("pages/product.ejs");})
+
+app.get("/accesories.ejs", function (req, res){
+    res.render("pages/accesories.ejs");})
+
+/*
+app.get("/index.html", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/index.html"))
+    }); 
+    
 app.get("/", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/index.html"))
-});
+    }); 
 
-app.get("/register.html", function (req, res){
-    res.sendFile (path.resolve (__dirname, "./views/register.html"))
-});
-
-app.get("/login.html", function (req, res){
-    res.sendFile (path.resolve (__dirname, "./views/login.html"))
+app.get("/accesories.html", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/accesories.html"))
 });
 
 app.get("/productCart.html", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/productCart.html"))
 });
 
+app.get("/register.html", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/register.html"))
+});
+
 app.get("/productDetail.html", function (req, res){
     res.sendFile (path.resolve (__dirname, "./views/productDetail.html"))
 });
 
-app.get("/accesories.html", function (req, res){
-    res.sendFile (path.resolve (__dirname, "./views/accesories.html"))
+app.get("/productCart.html", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/productCart.html"))
 });
 
-app.get("/index.html", function (req, res){
-    res.sendFile (path.resolve (__dirname, "./views/index.html"))
-    });
+app.get("/login.html", function (req, res){
+    res.sendFile (path.resolve (__dirname, "./views/login.html"))
+});
 
-/*res.render("pages/index.html");});*/
+*/
 
 
-app.set("view engine", "ejs");
+    
+
 
 
