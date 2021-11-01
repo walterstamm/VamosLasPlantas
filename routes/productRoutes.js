aa41ca3d37fbcfc89faba6fc69d8efee412a4e40
walterstamm <walterstamm92@gmail.com>
2021-11-01 00:52:13 -0300

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -24,7 +24,7 @@ const upload=multer({storage});
 
 /*2*/ router.get('/nuevosProd',controller.nuevosProd);
 
-/*3*/ router.get('/product/:id', controller.detalleProduct);
+/*3*/ router.get('/:id', controller.detalleProduct);
 
 /*4*/ router.post('/products',upload.single('imagen'),controller.guardarNuevo);
 
