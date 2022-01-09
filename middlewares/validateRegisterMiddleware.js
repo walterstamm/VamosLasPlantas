433ee9818f1dd77284fa68adc9f4de73c40360a9
marianojs86 <marianojs86@hotmail.com>
2022-01-09 17:09:07 -0300

    retro sprint 7, mas validaciones

--- middlewares/validateRegisterMiddleware.js
+++ middlewares/validateRegisterMiddleware.js
@@ -1,25 +1,28 @@
 const path = require('path');
 const {body} = require ('express-validator');
 
-module.exports = [
-    body ('user_nombre').notEmpty().isLength({min: 2}).withMessage('Ingresá tu nombre'),
-    body ('user_apellido').notEmpty().isLength({min: 2}).withMessage('Ingresá tu apellido'),
-    body ('user_email').notEmpty().withMessage('Ingresá tu correo electrónico').bail().isEmail().withMessage('Debes escribir un formato de correo válido'),
-    body ('user_password').notEmpty().isLength({min: 8}).withMessage('Tienes que escribir una contraseña'),
-    body ('user_foto').custom((value, { req }) => {
-        let file = req.file;
-        let acceptedExtensions = ['.jpg','.jpeg', '.png', 'gif'];
+module.exports = [    
+            body ('user_nombre').notEmpty().withMessage('Ingresá tu nombre')
+        .isLength({ min: 2 }).withMessage('Ingresá mínimo 2 carácteres'),
+        body ('user_apellido').notEmpty().withMessage('Ingresá tu apellido')
+        .isLength({min: 2}).withMessage('Ingresá mínimo 2 carácteres'),
+        body ('user_email').notEmpty().withMessage('Ingresá tu correo electrónico').bail()
+        .isEmail().withMessage('Debes escribir un formato de correo válido'),
+        body ('user_password').notEmpty().withMessage('Tienes que escribir una contraseña').isLength({min: 8})
+        .withMessage('La contraseña debe tener mínimo 8 carácteres')
+        //body ('user_foto').custom((value, { req }) => {
+        //    let file = req.file;
+        //    let acceptedExtensions = ['.jpg', '.jpeg', '.png', '.gif'];
 
-        if (!file) {
-            throw new Error('Tenes que subir una imagen');
-        } else {
-            let fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
-
-            }
-        }
-        return true;
-    })
+        //    if (!file) {
+        //        throw new Error('Tenes que subir una imagen');
+        //    } else {
+        //        let fileExtension = path.extname(file.originalname);
+        //        if (!acceptedExtensions.includes(fileExtension)) {
+         //           throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+          //      }
+          //  }
+          //  return true;
+      //  })
 
 ]
--- middlewares/validationUpDateMiddleware.js
+++ middlewares/validationUpDateMiddleware.js
@@ -2,5 +2,5 @@ const path = require('path');
 const {body} = require ('express-validator');
 
 module.exports = [
-    body ('email').notEmpty().withMessage('Ingresá tu correo electrónico').bail().isEmail().withMessage('Debes escribir un formato de correo válido')
+    body ('email').notEmpty().withMessage('Ingresá tu correo electrónico').isEmail().withMessage('Debes escribir un formato de correo válido')
 ]
